173dce6e2119f775fa5e20cc32daf47cea2944b1
Chris Teku <chris.teku@ensta-paris.fr>
2025-08-15 11:27:40 +0200

    Changements

--- greeks.py
+++ greeks.py
@@ -1,20 +1,20 @@
 import numpy as np
 from scipy.stats import norm
 
-def compute_greeks(option_type: str, S: float, K: float, r: float, sigma: float, T: float):
-    """
-    Compute the Delta, the Gamma et the Vega of a call option.
-    
-    """
-    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
-    delta = norm.cdf(d1) if option_type == 'call' else -norm.cdf(-d1)
-    gamma = norm.pdf(d1) / (S * sigma * np.sqrt(T))
-    vega = S * norm.pdf(d1) * np.sqrt(T)
+# Greeks
+def delta_call(S, K, T, r, sigma):
+    d1 = (np.log(S/K) + (r + 0.5*sigma**2) * T) / (sigma * np.sqrt(T))
+    return norm.cdf(d1)
 
-    return {
-        'Delta': delta,
-        'Gamma': gamma,
-        'Vega': vega
-    }
+def delta_put(S, K, T, r, sigma):
+    d1 = (np.log(S/K) + (r + 0.5*sigma**2) * T) / (sigma * np.sqrt(T))
+    return norm.cdf(d1) - 1
 
+def gamma(S, K, T, r, sigma):
+    d1 = (np.log(S/K) + (r + 0.5*sigma**2) * T) / (sigma * np.sqrt(T))
+    return norm.pdf(d1) / (S * sigma * np.sqrt(T))
+
+def vega(S, K, T, r, sigma):
+    d1 = (np.log(S/K) + (r + 0.5*sigma**2) * T) / (sigma * np.sqrt(T))
+    return S * norm.pdf(d1) * np.sqrt(T)
 
--- visualization.py
+++ visualization.py
@@ -2,23 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 from pricing import black_scholes_price
+from greeks import delta_call
+from greeks import delta_put
+from greeks import gamma
+from greeks import vega
 
-# Greeks
-def delta_call(S, K, T, r, sigma):
-    d1 = (np.log(S/K) + (r + 0.5*sigma**2) * T) / (sigma * np.sqrt(T))
-    return norm.cdf(d1)
-
-def delta_put(S, K, T, r, sigma):
-    d1 = (np.log(S/K) + (r + 0.5*sigma**2) * T) / (sigma * np.sqrt(T))
-    return norm.cdf(d1) - 1
-
-def gamma(S, K, T, r, sigma):
-    d1 = (np.log(S/K) + (r + 0.5*sigma**2) * T) / (sigma * np.sqrt(T))
-    return norm.pdf(d1) / (S * sigma * np.sqrt(T))
-
-def vega(S, K, T, r, sigma):
-    d1 = (np.log(S/K) + (r + 0.5*sigma**2) * T) / (sigma * np.sqrt(T))
-    return S * norm.pdf(d1) * np.sqrt(T)
 
 # --- Visualization of the payoff ---
 def plot_payoff(S_range, K, option_type="call"):
